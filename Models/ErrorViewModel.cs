d7a44fef1fda2c5b5a31096d462aac2eaa6e614e
carlos <soscarlos@gmail.com>
2023-01-19 14:38:33 +0100

    second commit add elements of the application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myfirstaspmvcapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
